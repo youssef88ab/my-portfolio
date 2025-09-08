601f0370ddf3edae9f37b8477e2055b3a51fe377
YOUSSEF ABOU ELJIHAD <yousef.aboueljihad@gmail.com>
2025-09-08 13:40:33 +0100

    maybe last fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import { type JSX } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Projects from "./pages/projects";
 import BlogPage from "./pages/blogs";
