4d1faf2f5b91f3d7e0b34bdbf688759ca7f9d722
YOUSSEF ABOU ELJIHAD <yousef.aboueljihad@gmail.com>
2025-09-08 13:27:51 +0100

    psuh

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }: { command: 'serve' | 'build' }) => {
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
     base: "/my-portfolio",
