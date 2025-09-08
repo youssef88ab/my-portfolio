997c577c327c5d4de940655eb0021586727d89a2
YOUSSEF ABOU ELJIHAD <yousef.aboueljihad@gmail.com>
2025-09-08 13:25:20 +0100

    psuh

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ command }: { command: 'serve' | 'build' }) => {
   return {
     plugins: [react(), tailwindcss()],
-    base: command === 'build' ? '/' : '/', // ✅ dev vs deploy
+    base: "/my-portfolio",
   }
 })
